58dff431bad58a72b05a0db5eebe2108c85f916a
Junhyunny <kang3966@naver.com>
2022-01-23 20:26:01 +0900

    Build Dockerfile 추가, nginx.conf 설정 추가, proxy middleware 추가

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/backend_service',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: 'http://192.168.0.2:8080',
             changeOrigin: true,
             pathRewrite: {
                 '^/backend_service': ''
